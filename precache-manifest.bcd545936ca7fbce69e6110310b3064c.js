05c3d5d37e2327fa14666178e64666a05a189bc8
raviteja <rteja7228@gmail.com>
2018-11-10 09:25:56 +0530

    Updates

---
+++ precache-manifest.bcd545936ca7fbce69e6110310b3064c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-cart/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b509cb4f645738d95a2a",
+    "url": "/shopping-cart/static/js/runtime~main.b509cb4f.js"
+  },
+  {
+    "revision": "2e2df81369b2bd12f3d4",
+    "url": "/shopping-cart/static/js/main.2e2df813.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/shopping-cart/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "2e2df81369b2bd12f3d4",
+    "url": "/shopping-cart/static/css/main.ea006e65.chunk.css"
+  },
+  {
+    "revision": "419ddb2d781d613e2b0e395e537eaffe",
+    "url": "/shopping-cart/index.html"
+  }
+];
